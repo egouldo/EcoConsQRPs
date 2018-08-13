b6c31c0aca60c4a8e1ebdf8d70090f12042387ec
egouldo <elisebgould@gmail.comssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCxi6HD7jiZr1vVPVA/BBdiRc/tb8nZqArQrdowaBfK24ZqwC9bc69VbVZqWc9kuAdrrXZuxsNw3CTuB+aLkXnNA3ATTtMsZ6B8WYcQLbrP4kbLB7kAjsnsCFXs0U1SxrfiQM6Z0/hsum14Q2PGJY0cTBidc6Dj5kFlCRtFiIxGnmgT7KNY1uhu0jZ0oCTLO9yQ6MvjsCib40uhONK+TDXhWhJ0+F2lWAqfleti3xoBF3eoQxGYpBQCIUXF/ZSSrrAnPT2oIlneesGZmLMemr9a+4+YyT99keae6WNoPs4qhIScdtrowry5IWe9D61J/VJpmWFr7zKMKo+vCmYe+EWL elisebgould@gmail.com>
2018-08-13 15:32:53 +1000

    make data long and add expert/non-expert key/id vars

--- R/generate_synthetic_data.R
+++ R/generate_synthetic_data.R
@@ -35,7 +35,7 @@ generate_synthetic_survey_data <- function(n_participants = 1000) {
           )
   # Per QRP questions
   survey_responses <- 
-          dplyr::left_join({
+          dplyr::bind_rows({
                   survey_responses %>% 
                           unnest(expert_method) %>%
                           dplyr::left_join(., QRP_key, by = c("expert_method" = "method")) %>%
@@ -46,9 +46,10 @@ generate_synthetic_survey_data <- function(n_participants = 1000) {
                                         comments = wakefield::lorem_ipsum(n = last(QRP_key)),
                                         consequence_best = list(gen_impact_values()),
                                         consequence_worst = list(gen_impact_values()),
-                                        consequence_average = list(gen_impact_values())) %>%
+                                        consequence_average = list(gen_impact_values()),
+                                        expert_status = "expert") %>%
                           ungroup() %>%
-                          nest(-response_id, -date, .key = expert_method_responses)},
+                          dplyr::rename(method = expert_method)},
                   
                   {survey_responses %>% 
                                   unnest(non_expert_method) %>%
@@ -60,10 +61,10 @@ generate_synthetic_survey_data <- function(n_participants = 1000) {
                                                 comments = wakefield::lorem_ipsum(n = last(QRP_key)),
                                                 consequence_best = list(gen_impact_values()),
                                                 consequence_worst = list(gen_impact_values()),
-                                                consequence_average = list(gen_impact_values())) %>%
+                                                consequence_average = list(gen_impact_values()),
+                                                expert_status = "non_expert") %>%
                                   ungroup() %>%
-                                  nest(-response_id, -date, .key = non_expert_method_responses)},
-                  by = c("response_id", "date")
+                                  dplyr::rename(method = non_expert_method)}
           )
 }
 
