30531e2997391aa1716ff26b5111bcb883d4e10b
egouldo <egouldo@users.noreply.github.com>
2025-07-29 16:17:24 +1000

    functionalise data tidying code

--- R/make_table_data.R
+++ R/make_table_data.R
@@ -1,18 +1,19 @@
 # Prepare Data for Visualisation
 
-qrp_table_data <- 
-        qrp_data %>% 
-        mutate(model_phase = str_split(model_phase, ", "), 
-               model_subphase = str_split(model_subphase, ", ")) %>% 
-        rowwise() %>% # Ensure we are working with each row independently
-        arrange(model_phase, model_subphase) %>% 
-        filter(length(model_phase) > 1 && length(model_phase) == length(model_subphase) | length(model_phase) == 1) %>%  # temporary hack to ensure equal lengths
-        unnest(c(model_phase, model_subphase)) %>% 
-        group_by(practice_coded) %>% 
-        unite(col = model_phase_subphase, model_phase, model_subphase, sep = " - ") %>% 
-        mutate(values = TRUE) %>% 
-        pivot_wider(names_from = model_phase_subphase, 
-                    values_from = values)
-
-
-        
+make_qrp_table_data <- function(qrp_data) {
+  qrp_table_data <- 
+    qrp_data %>% 
+    mutate(model_phase = str_split(model_phase, ", "), 
+           model_subphase = str_split(model_subphase, ", ")) %>% 
+    rowwise() %>% # Ensure we are working with each row independently
+    arrange(model_phase, model_subphase) %>% 
+    filter(length(model_phase) > 1 && length(model_phase) == length(model_subphase) | length(model_phase) == 1) %>%  # temporary hack to ensure equal lengths
+    unnest(c(model_phase, model_subphase)) %>% 
+    group_by(practice_coded) %>% 
+    unite(col = model_phase_subphase, model_phase, model_subphase, sep = " - ") %>% 
+    mutate(values = 1) %>% 
+    pivot_wider(names_from = model_phase_subphase, 
+                values_from = values, values_fill = 0)
+  
+  return(qrp_table_data)
+}
--- R/tidy_QRP_database.R
+++ R/tidy_QRP_database.R
@@ -1,15 +1,15 @@
 # Read and Tidy Database Data
 
-library(tidyverse)
-library(here)
-library(janitor)
-
-qrp_data <- readr::read_csv(here::here("data", "QRP_database.csv")) %>% 
-        janitor::clean_names() %>% 
-        rename(source = source_13) %>% 
-        filter(source_1 != "Internal") %>% 
-        select(-source_1, -source_file, -ends_with("_source")) %>% 
-        drop_na(model_phase, target, practice_coded) %>% 
-        mutate(source = str_remove(source, "\\[") %>% 
-                       str_remove("\\]") %>% 
-                       str_remove("source_notes/"))
+tidy_QRP_database <- function(database_path) {
+        qrp_data <- readr::read_csv(database_path) %>% 
+                janitor::clean_names() %>% 
+                rename(source = source_13) %>% 
+                filter(source_1 != "Internal") %>% 
+                select(-source_1, -source_file, -ends_with("_source")) %>% 
+                drop_na(model_phase, target, practice_coded) %>% 
+                mutate(source = str_remove(source, "\\[") %>% 
+                               str_remove("\\]") %>% 
+                               str_remove("source_notes/"))
+        
+        return(qrp_data)
+}
