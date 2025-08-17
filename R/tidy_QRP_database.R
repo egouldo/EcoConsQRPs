ca331ec1fa75d07bd5d4484adbbc72f7c04cbf65
egouldo <egouldo@users.noreply.github.com>
2025-08-17 19:18:47 +1000

    Remove extra square brackets from all _source columns.

--- R/tidy_QRP_database.R
+++ R/tidy_QRP_database.R
@@ -5,12 +5,15 @@ tidy_QRP_database <- function(database_path) {
                 janitor::clean_names() %>% 
                 rename(source = source_15) %>% 
                 filter(source_1 != "Internal") %>% 
-                select(-source_1, -source_file, -ends_with("_source")) %>% 
+                select(-source_1, 
+                       -source_file#, 
+                       # -ends_with("_source")
+                       ) %>% 
                 drop_na(model_phase, target, practice_coded) %>% 
-                mutate(source = str_remove(source, "\\[") %>% 
-                               str_remove("\\]") %>% 
-                               str_remove("source_notes/"),
-                       across(starts_with("model_"), ~ stringr::str_trim(.x))) 
-        
+                mutate(across(ends_with("source"), 
+                              ~ str_remove(.x, "\\[") %>% 
+                                      str_remove("\\]") %>% 
+                                      str_remove("source_notes/")),
+                       across(everything(), ~ stringr::str_trim(.x))) 
         return(qrp_data)
 }
