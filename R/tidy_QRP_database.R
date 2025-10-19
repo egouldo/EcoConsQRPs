4c9b1379a5b7f38fe1bfcb5ac3d9fb82c1827b0b
egouldo <egouldo@users.noreply.github.com>
2025-10-19 21:08:43 +1100

    Switch to 12 Oct revised definitions and reexecute roadmap viz: table 2 and fig 3

--- R/tidy_QRP_database.R
+++ R/tidy_QRP_database.R
@@ -3,12 +3,12 @@
 tidy_QRP_database <- function(database_path) {
         qrp_data <- readr::read_csv(database_path) %>% 
                 janitor::clean_names() %>% 
-                rename(source = source_15) %>% 
-                filter(source_1 != "Internal") %>% 
-                select(-source_1, 
-                       -source_file#, 
-                       # -ends_with("_source")
-                       ) %>% 
+                # rename(source = source_15) %>% 
+                # filter(source_1 != "Internal") %>% 
+                # select(-source_1, 
+                #        -source_file#, 
+                #        # -ends_with("_source")
+                #        ) %>% 
                 drop_na(model_phase, target, practice_coded) %>% 
                 mutate(across(ends_with("source"), 
                               ~ str_remove(.x, "\\[") %>% 
