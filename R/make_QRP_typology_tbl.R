cc54ad086f3dbb8c8f22517e1df85d369a63c4ea
egouldo <egouldo@users.noreply.github.com>
2025-11-16 19:02:34 +1100

    Insert remainder of updated draft, and move QRP typology table code externally

---
+++ R/make_QRP_typology_tbl.R
@@ -0,0 +1,91 @@
+# QRP typology table
+library(tidyverse)
+library(glue)
+library(gt)
+
+typology <- tibble(
+        Decision = c(
+                "A priori",
+                "A priori",
+                "A priori",
+                "Defensible",
+                "Defensible",
+                "Defensible",
+                "Questionable",
+                "Questionable",
+                "Questionable"
+        ),
+        Constraint = c(
+                "$\\phi(T) \\in \\Phi_{\\text{fixed}}$",
+                "$\\phi(T) \\in \\Phi_{\\text{fixed}}$",
+                "$\\phi(T) \\in \\Phi_{\\text{fixed}}$",
+                "$\\phi_D(T) \\in \\Phi(\\Omega)$",
+                "$\\phi_D(T) \\in \\Phi(\\Omega)$",
+                "$\\phi_D(T) \\in \\Phi(\\Omega)$",
+                "$\\phi_Q(T) \\in \\Phi(R^*)$",
+                "$\\phi_Q(T) \\in \\Phi(R^*)$",
+                "$\\phi_Q(T) \\in \\Phi(R^*)$"
+        ),
+        Motivation = c(
+                "",
+                "",
+                "",
+                "*Systematic evaluation according to predetermined criteria*",
+                "*Systematic evaluation according to predetermined criteria*",
+                "*Systematic evaluation according to predetermined criteria*",
+                "*Optimising for preferred results $R^*$, rather than assessment*",
+                "*Optimising for preferred results $R^*$, rather than assessment*",
+                "*Optimising for preferred results $R^*$, rather than assessment*"
+        ),
+        Prespec_Reporting = c(
+                "$S_{\\text{pre}}(\\{C(X,M,\\phi\\})$",
+                "$S_{\\text{pre}}(\\{C(X,M,\\phi(M_j))\\})$",
+                "Decisions affecting the model $\\phi$ or model outputs $\\phi(M_j)$, and which analyses and results will be reported, are decided a priori.",
+                "$S_{\\text{pre}}(\\{C(X,M;\\phi_D\\})$",
+                "$S_{\\text{pre}}(\\{C(X,M;\\phi_D(M_j))\\})$",
+                "Data-dependent and defensible modelling decisions $C(\\boldsymbol{X}, T;\\phi_D)$, with pre-specified reporting choices $S_{\\text{pre}}$.",
+                "Model Fishing: $C(X,M;\\phi_Q(M))$",
+                "S-hacking: $C(X,M;\\phi_Q(M_j))$",
+                "Modelling and analysis decisions are opportunistic and therefore questionable."
+        ),
+        Selective_Reporting = c(
+                "Selective Reporting:<br>$S_Q\\{C(X,M,\\phi\\})$",
+                "$S_Q\\{C(X,M,\\phi(M_j))\\})$",
+                "A priori analysis decisions about the model $\\phi$ and model outputs $\\phi(M_j)$, but reporting choices are data-dependent, and are selectively reported $S_Q$",
+                "Selective Reporting:<br>$S_Q(\\{C(X,M;\\phi_D\\})$",
+                "$S_Q(\\{C(X,M;\\phi_D(M_j))\\})$",
+                "Modelling decisions are data-dependent and defensible, but subject to selective reporting.",
+                "By definition, desired outcomes $R^*$ and $\\phi_Q$ are undisclosed, and may be tacit rather than explicit.",
+                "",
+                ""
+        )
+) %>% 
+        mutate(Decision = glue::glue("{Decision} analytic decision, where:"))
+
+typology_tbl <- typology %>%
+        group_by(Decision, Constraint, Motivation) %>% 
+        gt(row_group_as_column = TRUE, 
+           process_md = TRUE, 
+           row_group.sep = "<br>"
+        ) %>%
+        cols_label(
+                Decision = "**Decision Type**",
+                Constraint = "**Constraint Set**",
+                Motivation = "**Motivation**",
+                Prespec_Reporting = "**With Pre-specified Reporting $S_{\\text{pre}}$**",
+                Selective_Reporting = "**With Selective Reporting $S_Q$**"
+        ) %>%
+        gtExtras::gt_theme_538(quiet = TRUE, ) %>% 
+        fmt_markdown(columns = everything()) %>%
+        cols_align(align = "left", columns = everything()) %>% 
+        gt::cols_label_with(fn = gt::md) %>% 
+        tab_stubhead(label = gt::md("**Analytic Decision Type and Motivation**")) %>% 
+        gt::tab_spanner(label = "Reporting Type") %>% 
+        gt::tab_style(style = cell_borders(style = "hidden",sides = "bottom"), 
+                      locations = cells_body(rows = c(1,2,4,5,7,8), 
+                                             columns = everything())) %>% 
+        gt::tab_style(style = cell_text(v_align = "top"),
+                      locations = cells_body(columns = everything(), 
+                                             rows = c(3,6)))
+
+gt::gtsave(typology_tbl, path = here::here("tables/"), filename = "QRP_typology.png")
